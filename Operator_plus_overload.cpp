3925853d2b7187ce8ba26819a34e765b8c03dd7d
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-07-16 12:23:45 +0300

    ==, !=, ++,--,[] overload

--- Operator_plus_overload.cpp
+++ Operator_plus_overload.cpp
@@ -5,6 +5,7 @@ class Point
 private:
     int x;
     int y;
+   
 public:
     Point()
     {
@@ -15,6 +16,11 @@ public:
     {
         this->x = x;
         this->y = y;
+    }
+    void ShowObj()
+    {
+        cout << "x= " << x << endl;
+        cout << "y= " << y << endl;
     }
     Point operator+ (const Point& other)
     {
@@ -32,10 +38,46 @@ public:
    {
        return !(this->x == other.x && this->y == other.y);
    }
-    void ShowObj()
+
+   Point& operator++()//Prefix increment
+   {
+       this->x++;
+       this->y++;
+       return *this;
+   }
+   Point& operator ++(int value)//Postfix increment
+   {
+       Point temp(*this);
+       this->x++;
+       this->y++;
+
+       return temp;
+   }
+   Point& operator--()//Prefix decrement
+   { 
+       this->x--;
+       this->y--;
+       return *this;
+   }
+   Point& operator --(int value)//Postfix decrement
+   {
+       Point temp(*this);
+       this->x--;
+       this->y--;
+
+       return temp;
+   }
+
+   
+};
+class Array
+{
+private:
+    int arr[5] = { 23,44,67,90,33 };
+public:
+    int& operator [](int index)//Overload []
     {
-        cout <<"x= "<< x << endl;
-        cout <<"y= "<< y << endl;
+        return arr[index];
     }
 };
 
@@ -68,6 +110,26 @@ int main()
     {
         cout << "B==C" << endl;
     }
+    ++a;
+    ++b;
+    ++c;
+    cout << "Object c after prefix increment: " << endl;
+    c.ShowObj();
+    c++;
+    cout << "Object c after postfix increment: " << endl;
+    c.ShowObj();
+    --c;
+    cout << "Object c after prefix decrement: " << endl;
+    c.ShowObj();
+    c--;
+    cout << "Object c after postfix decrement: " << endl;
+    c.ShowObj();
+
+    Array arr;
+    cout <<"Arr[0]= "<< arr[0] << endl;
+    arr[0] = 9;
+    cout << "Arr[0]= " << arr[0] << endl;
+
 
     return 0;
 }
